114364385bfa41c5093a19621a0125284ebfc455
Shraddha <jadhavshradddha5@gmail.com>
2023-02-07 09:14:09 +0530

    This is uc2.To allow the player to choose a letter

--- src/com/BridgeLab/WorkShop/TicTacToeGame.java
+++ src/com/BridgeLab/WorkShop/TicTacToeGame.java
@@ -1,12 +1,34 @@
 package com.BridgeLab.WorkShop;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
    char [] board=new char[10];
+   char chooseLetter;
+   char computerLetter;
+   //UC1- creating empty board
     public void createBoard()
     {
         for(int i=1;i< board.length;i++)
         {
             board[i]=' ';
+        }
+    }
+    //UC2-Ability to allow the
+    //player to choose a
+    //letter X or O
+    public void chooseLetter()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Choose a letter: X or O :");
+        chooseLetter=sc.next().toUpperCase().charAt(0);
+        if(chooseLetter=='X')
+        {
+            computerLetter='X';
+        }
+        else
+        {
+            computerLetter='O';
         }
     }
 }
